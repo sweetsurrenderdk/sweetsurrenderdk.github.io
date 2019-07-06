5fde312bcf47926135a65446732e70cc74499308
Sweet Surrender <sweetsurrenderdk@users.noreply.github.com>
2019-07-06 11:36:52 +0200

    Update navigation.php

--- views/inc/main/navigation.php
+++ views/inc/main/navigation.php
@@ -3,9 +3,11 @@
 if($page->language == 'da') {
     $home_url = '/da/hjem/';
     $opposite_language = 'en';
+    $top_text = 'Sommerlukket fra 7. juli til og med 4. august'
 } else {
     $home_url = '/en/home/';
     $opposite_language = 'da';
+    $top_text = 'Summer closed from July 7th to August 4th'
 }
 
 $all_pages = HashBrown\get_all_contents();
@@ -83,4 +85,5 @@ foreach($all_pages as $p) {
         </div>
     </div> 
 </div>
+<div style="position: fixed; width: 100%; background-color: #FFF; z-index: 50; background-color: yellow; font-size: large; font-weight: bold; text-align: center;">{{ top_text }}</div>
 <button class="backdrop" onclick="closeWidgets();"></button>
